abde54e0733273431d5d038b5265563add095404
shashi <shashipanta57@gmail.com>
2023-11-03 20:22:09 +0545

    init:Created skeleton for websockets implementation in spring boot
    Dependency used:
    * spring-boot-starter-websocket
    * spring-boot-starter-web
    * spring-boot-starter-security
    
    Database used:
    PostgreSQL

---
+++ src/main/java/com/notification/springnotification/SpringnotificationApplication.java
@@ -0,0 +1,13 @@
+package com.notification.springnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringnotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringnotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notification/springnotification/SpringnotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notification.springnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringnotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
