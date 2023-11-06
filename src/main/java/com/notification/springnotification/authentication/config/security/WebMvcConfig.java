94ce927e4068adb2e4406c0c724eb3c7a92cffc2
shashi <shashipanta57@gmail.com>
2023-11-07 01:29:02 +0545

    initial: Updated cors configuration

--- src/main/java/com/notification/springnotification/authentication/config/security/WebMvcConfig.java
+++ src/main/java/com/notification/springnotification/authentication/config/security/WebMvcConfig.java
@@ -1,15 +1,10 @@
 package com.notification.springnotification.authentication.config.security;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 /**
  * @author shashi
@@ -19,13 +14,13 @@ import java.util.Arrays;
 @Configuration
 public class CorsConfiguration {
 
-    private static final String[] ALLOWED_METHODS = {"GET", "POST", "PUT", "DELETE", "OPTIONS"};
-    private static final String[] ALLOWED_HEADERS = {"Content-Type", "x-request-with"};
-    private static final int MAX_AGE = 3600;
+    private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN";
+    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
+    private static final Long MAX_AGE = 3600L;
 
-    private static final String ALLOWED_ORIGIN = "http://localhost:8080";
+    private static final String ALLOWED_ORIGIN = "http://localhost:5173";
 
-    @Value("$cors.allowed-origins")
+    @Value("${cors.allowed-origins}")
     private String allowedOrigin;
 
     @Bean
@@ -34,10 +29,11 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry
-                        .addMapping("*/*")
-                        .allowedOrigins(allowedOrigin)
+                        .addMapping("/**")
+                        .allowedOrigins(ALLOWED_ORIGIN)
                         .allowedHeaders(ALLOWED_HEADERS)
                         .allowedMethods(ALLOWED_METHODS)
+                        .exposedHeaders(ALLOWED_HEADERS)
                         .maxAge(MAX_AGE);
             }
         };
