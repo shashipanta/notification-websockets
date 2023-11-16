6d2df295ddf67f1419701eb06739c7ed4659621a
Shashi Panta <64604171+shashipanta@users.noreply.github.com>
2023-11-16 18:55:10 +0545

    Merged security to develop (#7)
    * initial: Added application.yaml for database
    
    * initial: Added Validator for bean validation
    
    Added spring-boot-starter-validation
    
    * feat(initial-setup): added global configuration : base controller, common exceptions
    
    BaseController for common controller
    
    status,exceptions are also added as part of global setup
    
    * initial: added string representation for Status enum
    
    AbstractAuditor had Status column that mapped ordinal instead of String
    
    * init:spring security logging
    
    * initial: Added cors configuration
    
    * initial: Updated cors configuration

---
+++ src/main/java/com/notification/springnotification/global/controller/BaseController.java
@@ -0,0 +1,24 @@
+package com.notification.springnotification.global.controller;
+
+import com.notification.springnotification.global.GlobalApiResponse;
+
+/**
+ * @author shashi
+ * @version 1.0.0
+ * @since 11/4/23 1:10 AM
+ */
+public class BaseController {
+
+    private final String SUCCESS = "success";
+    private final String FAILURE = "failure";
+
+    public GlobalApiResponse successResponse(String message, Object data) {
+        return GlobalApiResponse
+                .builder()
+                .statusCode(SUCCESS)
+                .data(data)
+                .message(message)
+                .build();
+
+    }
+}
---
+++ src/main/java/com/notification/springnotification/global/models/AbstractAuditor.java
@@ -0,0 +1,49 @@
+package com.notification.springnotification.global.models;
+
+import com.notification.springnotification.global.enums.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.sql.Timestamp;
+
+/**
+ * @author shashi
+ * @version 1.0.0
+ * @since 11/3/23 10:35 PM
+ */
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractAuditor {
+
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private Integer createdBy;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Timestamp createdAt;
+
+    @LastModifiedBy
+    @Column(name = "last_modified_by")
+    private Integer lastModifiedBy;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_at")
+    private Timestamp lastModifiedDate;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = Boolean.TRUE;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.APPROVED;
+}
